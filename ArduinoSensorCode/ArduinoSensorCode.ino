37a24886313e7bfe3f3880e0c8140d988a149dad
rossmattheww <rosscmatthew@gmail.com>
2019-11-25 23:38:54 -0500

    Updated Arduino servo/sensor code

--- ArduinoSensorCode/ArduinoSensorCode.ino
+++ ArduinoSensorCode/ArduinoSensorCode.ino
@@ -1,74 +1,74 @@
 #include <Servo.h>
 Servo servo;
-const int trigPin = 9;  //Trigger pin for HC-SR04 sensor
-const int echoPin = 10;  //Echo pin for HC-SR04 sensor
+int trigPin = 2;  //Trigger pin for HC-SR04 sensor
+int echoPin = 3;  //Echo pin for HC-SR04 sensor
 int backPlateButton = A0;  //Button on shelf back plate
 int stockButton = A1;
 int stockButtonState = 0;
 int backButtonState = 0;  //Button initial value (Active high)
 long duration;  //Duration time for return signal on sensor
-int distance;
-float range, minRange;
-boolean stocking = false;
+double distance, distanceOld, range, minRange;
 
 void setup() {
-  //Setting the pin directions
-  pinMode(backPlateButton, INPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
   //Serial port communication begin
   Serial.begin(9600);
+  //Setting the pin directions
+  pinMode(backPlateButton, INPUT);
+  pinMode(stockButton, INPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   //Attach servo
   servo.attach(9);
-  range = 15.0;
-  minRange = 4.0;
+  range = 35.0;
+  minRange = 4;
   distance = sensorOn();
   stockButtonState = digitalRead(stockButton);
 }
 
-//void loop() {
-//  // put your main code here, to run repeatedly:
-//  if(stocking = false){  //Not in restocking mode
-//    if(distance > minRange){  //Move items to the front of the shelf
-//      moveForward();
-//    }
-//    checkShelfGap();  //Always checking if there is a gap in the items on the shelf
-//    Serial.println(distance);
-//  }
-//  if(stockButtonState == LOW){  //Shelf is being restocked
-//    moveBack();
-//  }
-//  if(outsideRange(range)){
-//    abort;
-//  }
-//}
+void loop() {
+  while(outsideRange(range)){   //Sensor is giving a bad reading
+    servo.detach();
+    delay(20);
+    //Serial.println("rechecking");
+  }
+  //Save previous and current distance so it can be seen if there is a change which the client PI needs to know about
+  distanceOld = distance;
+  distance = sensorOn();
 
-void loop(){
-  float temp = distanceAfterDelay(200);
-  Serial.println(temp);
-  outsideRange(range);
-  insideRange(range);
-  Serial.println("Completed");
-  delay(3000);
+  while(stockButtonState == HIGH){  //If restocking button is being pressed
+    moveBack();
+  }
+  
+  while(distance > 15.0){  //Using 15.0 as the desired distance for testing purposes
+    Serial.println("moving forward");
+    moveForward();
+    distance = sensorOn();
+  }
+  if(distance > minRange && distance < 15.0){  //Check if the backing plate has entered the "sweet spot" where we want it to be (for testing)
+    Serial.println("hit");
+    servo.detach();
+  }
+  if(distance != distanceOld){  //If the distance has changed
+    Serial.println(distance); //Update the client PI
+  }
+  delay(500);
 }
 
 /*
  * Function to move the shelf to the back when it needs to be restocked when the restock button has been pressed
  */
 void moveBack(){
-  stocking = true;
   servo.attach(9);
   Serial.print("attached");  //Flag
   while(distance < range){
     Serial.print(distance);
     Serial.println("stocking");  //Flag
-    servo.write(20);;
-    delay(200);
-    distance = distance - 1;  //Used for testing
+    servo.write(-180);
+    distance = sensorOn();
   }
-  if(distance == range){
+  if(distance >= range){
+    servo.detach();
     delay(2000);
-    stocking = false;
   }
 }
 
@@ -80,6 +80,7 @@ void moveForward(){
     servo.attach(9);
   }
   servo.write(180);
+  //servo.writeMicroseconds(2000);
 }
 
 void checkShelfGap(){
@@ -102,45 +103,50 @@ void checkShelfGap(){
   }
   //delay(500);
 }
+/*
+ * Convert time response measurement from sensor to a unit
+ */
 
-float convertToDistance(long _duration){
-   return (float)_duration*0.0343/2;  //Convert duration to distance(cm)
+int convertToDistance(long _duration){
+   return (double)_duration*0.0343/2;  //Convert duration to distance(cm)
 }
 
-float sensorOn(){
+/*
+ * Get a distance reading
+ */
+double sensorOn(){
   digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
+  delayMicroseconds(5);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   
-  float temp = pulseIn(echoPin, HIGH);
+  double temp = pulseIn(echoPin, HIGH);
   return convertToDistance(temp);
 }
 
-boolean outsideRange(float _range){
-  if(sensorOn() > _range || sensorOn() < minRange){
-    Serial.println("outsideRange: Y");
+/*
+ * Check if outside of range
+ */
+boolean outsideRange(double _range){
+  if(sensorOn() > _range){
     return true;
   }
   else{
-    Serial.println("outsideRange: N");
     return false;
   }
 }
 
-boolean insideRange(float _range){
-  if(sensorOn() < _range && sensorOn() > minRange){
-    Serial.println("insideRange: Y");
-    return true;
-  }
-  else{
-    Serial.println("insideRange: N");
-    return false;
-  }
-}
+//boolean insideRange(double _range){
+//  if(sensorOn() < _range){
+//    return true;
+//  }
+//  else{
+//    return false;
+//  }
+//}
 
-float distanceAfterDelay(int _delay){
+double distanceAfterDelay(double _delay){
   delay(_delay);
   return convertToDistance(sensorOn());
 }
